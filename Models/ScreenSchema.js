c4245f47947a9ab0bac9449a74d3464f8fc8671c
abimanyu-abi <abisquare0215@gmail.com>
2023-11-21 13:36:15 +0530

    Movie project uploaded

---
+++ Models/ScreenSchema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const screenSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    seats: {
+        type: Array,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    screenType: {
+        type: String,
+        required: true
+    },
+    movieSchedules: [
+        {
+            movieId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Movie', 
+                required: true
+            },
+            showTime: String,
+            notAvailableSeats: [{
+                
+                col : Number,
+                seat_id : String,
+                price : Number
+                
+            }],
+            showDate: Date
+        }
+    ]
+});
+
+const Screen = mongoose.model('Screen', screenSchema);
+
+module.exports = Screen;
